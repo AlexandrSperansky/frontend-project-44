b5089b1d7a1371dae39304592bb0d984fa8c34c2
AlexandrSperansky <kopobka010@gmail.com>
2022-11-25 00:55:39 +0300

    add brain-even game

---
+++ bin/brain-even.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import playGame from '../src/index.js';
+
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEvenNumber = (num) => num % 2 === 0;
+
+const getQuestionAndAnswer = () => {
+  function getRandomInt(max = 25) {
+    return Math.round(Math.random() * max);
+  }
+  const question = getRandomInt();
+  const answer = isEvenNumber(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const initGameEven = () => {
+  playGame(rule, getQuestionAndAnswer);
+};
+
+initGameEven();
