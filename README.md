1376a1a26309ad063ed2ee79ef2b770a50b66f5e
AlexandrSperansky <kopobka010@gmail.com>
2022-11-27 04:36:11 +0300

    add brain-gcd

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Actions Status](https://github.com/AlexandrSperansky/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/AlexandrSperansky/frontend-project-44/actions)
 brain-even: https://asciinema.org/a/aq5s54TIeJQ7GDaoXsNb9EnI7
 brain-calc: https://asciinema.org/a/WJHxt6U463oGl8UdHmwyo3WCZ
+brain-gcd:  https://asciinema.org/a/Tabp0oLqvbTCXRbm5y4hdrfni
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,37 @@
+import playGame from "../index.js";
+
+const rule = 'Find the greatest common divisor of given numbers.';
+
+
+const getQuestionAndAnswer = () => {
+    const getRandomInt = (min = 1, max = 25) => {  
+    return Math.floor(Math.random() * (max - min) + min);
+    }
+    const antirandom = getRandomInt(1, 3)
+    const number1 = getRandomInt() * antirandom;
+    const number2 = getRandomInt() * antirandom; 
+    let greatestNumber = number1; 
+    let minNumber = number2;
+    if (number2 > number1) {
+    greatestNumber = number2;
+    minNumber = number1;
+}
+let answer;
+for (let i = 0; i < greatestNumber; i++){
+    if (number1 % i === 0 && number2 % i === 0) {
+    answer = `${i}`;
+    }
+    if (greatestNumber % minNumber === 0) {
+        answer = `${minNumber}`
+    }
+}
+const question = `${number1} ${number2}`;
+
+    return [question, answer];
+}
+const initGamegcd = () => {
+    playGame(rule, getQuestionAndAnswer);
+};
+
+initGamegcd();
+export default initGamegcd;
